72449cf320330218d1d8da88f74f5e1b2cc9a9bc
23btrct005-coder <23btrct005@jainuniversity.ac.in>
2026-04-24 22:09:08 +0530

    Test: Hello World deployment test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Catalog from './pages/Catalog';
-import ProductDetail from './pages/ProductDetail';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Cart from './pages/Cart';
-import UserDashboard from './pages/UserDashboard';
-import AdminDashboard from './pages/AdminDashboard';
-import OrderSuccess from './pages/OrderSuccess';
-import OrderDetail from './pages/OrderDetail';
-import { AuthProvider } from './context/AuthContext';
-import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
-    <Router>
-      <AuthProvider>
-        <CartProvider>
-          <div className="min-h-screen flex flex-col">
-            <Navbar />
-            <main className="flex-grow">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/catalog" element={<Catalog />} />
-                <Route path="/product/:id" element={<ProductDetail />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/dashboard" element={<UserDashboard />} />
-                <Route path="/admin" element={<AdminDashboard />} />
-                <Route path="/order-success/:id" element={<OrderSuccess />} />
-                <Route path="/order/:id" element={<OrderDetail />} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
-        </CartProvider>
-      </AuthProvider>
-    </Router>
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>Shree Tech - Deployment Test</h1>
+      <p>If you see this, the build pipeline is working!</p>
+    </div>
   );
 }
 
